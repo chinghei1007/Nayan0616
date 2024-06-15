0da5b9a6da2cbed1899e0027117175dcee40c0c6
LAM <chinghei1007@gmail.com>
2024-06-16 00:27:12 +0800

    0616 0025

--- Numbers_exercise.py
+++ Numbers_exercise.py
@@ -1,3 +1,3 @@
 '''Please generate a random number that ranging from 5000
 then guess the number
-The program should be able to tell the user if the guess is too small or large'''
+The program should be able to tell the user if the guess is too small or large   '''
